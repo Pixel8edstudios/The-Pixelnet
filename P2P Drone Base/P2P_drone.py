430b8f2382542728924e7adb81b1d706bcf51c58
Pixel8edstudios <>
2022-02-08 19:01:20 -0500

    Started work on fallback systems for socket handling

--- P2P Drone Base/P2P_drone.py
+++ P2P Drone Base/P2P_drone.py
@@ -1,13 +1,18 @@
-import socket 
 import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+import socket 
+import sys
 import time
-import random
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-logging.critical("NOTE")
+print("DOING SOMETHING")
 port = 5598
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+logging.debug("Stated sockets and imported modules")
+print("STILL DOING SOMETHING")
 try:
+    logging.debug("Attempting to bind first time")
     s.bind(("",port))
+    logging.debug("Bound successfully")
 except :
     logging.error("Could not bind default socket 5598, retrying.", exc_info=True)
     for counts in range (3):
@@ -16,10 +21,22 @@ except :
             logging.info("Successfully bound to port 5598 after error, continuing with normal operations.", exec_info=True)
             time.sleep(1)
         except:
-            logging.error("Switching to randomized port selection", exc_info=True)
+            logging.error("Switching to alternative port selection.", exc_info=True)
             break
-        for counts in range (3):
-            
-    
+        while port_found == 0:
+            for ports in range(5599, 5603):
+                try:
+                    s.bind(("",ports))
+                    ports = str(ports)
+                    logging.info(f"Bound to the port {ports}")
+                    port_found = 1
+                except:
+                    if ports == 5603:
+                        logging.critical("COULD NOT BIND TO ANY PORTS, SHUTTING DOWN.")
+                        sys.exit()
+                    logging.info(f"Could not bind to the port {ports}")
+while True:
+    pass
+
 
 
