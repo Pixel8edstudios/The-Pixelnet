52c2dd4bec07b9b8ffce2ac15094fe0773614e84
Pixel8edstudios <>
2022-02-09 17:18:51 -0500

    Solved a problem where the logger couldnt write to file and output

--- P2P Drone Base/P2P_drone.py
+++ P2P Drone Base/P2P_drone.py
@@ -1,9 +1,18 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-import socket 
 import sys
+
+console = logging.StreamHandler()
+file_handler = logging.FileHandler("drone.log", "w")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)-15s: %(name)s: %(levelname)s: %(message)s',
+    handlers = [file_handler, console]
+)
+import socket 
 import time
+hostname = socket.gethostname()
+address = socket.gethostbyname(hostname)
+logging.debug(f"THIS IS ADDRESS {address}")
 print("DOING SOMETHING")
 port = 5598
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,30 +20,32 @@ logging.debug("Stated sockets and imported modules")
 print("STILL DOING SOMETHING")
 try:
     logging.debug("Attempting to bind first time")
-    s.bind(("",port))
+    s.bind((address,port))
     logging.debug("Bound successfully")
-except :
+except:
     logging.error("Could not bind default socket 5598, retrying.", exc_info=True)
     for counts in range (3):
         try:
-            s.bind(("",port))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((address,port))
             logging.info("Successfully bound to port 5598 after error, continuing with normal operations.", exec_info=True)
             time.sleep(1)
         except:
             logging.error("Switching to alternative port selection.", exc_info=True)
-            break
-        while port_found == 0:
-            for ports in range(5599, 5603):
-                try:
-                    s.bind(("",ports))
-                    ports = str(ports)
-                    logging.info(f"Bound to the port {ports}")
-                    port_found = 1
-                except:
-                    if ports == 5603:
-                        logging.critical("COULD NOT BIND TO ANY PORTS, SHUTTING DOWN.")
-                        sys.exit()
-                    logging.info(f"Could not bind to the port {ports}")
+            port_found = 0
+            while port_found == 0:
+                for ports in range(5599, 5604):
+                    try:
+                        logging.debug(ports)
+                        s.bind((address,ports))
+                        ports = str(ports)
+                        logging.info(f"Bound to the port {ports}")
+                        port_found = 1
+                    except:
+                        logging.info(f"Could not bind to the port {ports} with address {address,ports}")
+                        if ports == 5603:
+                            logging.critical("COULD NOT BIND TO ANY PORTS, SHUTTING DOWN.", exc_info=True)
+                            sys.exit()
 while True:
     pass
 
